7b0d80325b2e84c9abcdcb7b0d063b87ee39c306
AmanMunawar <86243398+AmanMunawar@users.noreply.github.com>
2023-08-04 18:00:18 +0100

    Update FIndNearestStoreController.java

--- src/main/java/com/example/demo/controller/FIndNearestStoreController.java
+++ src/main/java/com/example/demo/controller/FIndNearestStoreController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class FIndNearestStoreController {
 
     @Autowired
-    private GrocerStoreInformationDao grocerStoreInformationDao;
+    private GrocerStoreInformationDao grocerStoreInrmationDao;
 
     // //later would come a
     // @GetMapping("/findnearestgrocerstore")
